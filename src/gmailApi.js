5db152a657e5ba4e643a6ae96a2cfa968dc9270f
Aniket-Shobhit <shobhitaniket3@gmail.com>
2022-08-25 11:00:18 +0530

    setting up oAuth2Client

---
+++ src/gmailApi.js
@@ -0,0 +1,12 @@
+const { google } = require('googleapis');
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN});
+
+module.exports = oAuth2Client;
---
+++ src/gmailSend.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+const oAuth2Client = require('./gmailApi');
+
+async function sendMail(receiver, subject, text, htmlTemplate) {
+    try {
+        const accessToken = await oAuth2Client.getAccessToken()
+
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: process.env.user,
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken
+            }
+        }); 
+
+        const mailOptions = {
+            from: '"Aniket" <shobhitaniket3@gmail.com>',
+            to: receiver,
+            subject: subject,
+            text: text,
+            html: html,
+        };
+
+        const result = await transport.sendMail(mailOptions);
+        return result;
+
+    } catch(e) {
+        return e;
+    }
+}
+
+sendMail().then(result => console.log('Email sent successfully!', result))
+.catch(err => console.log(err.message));
