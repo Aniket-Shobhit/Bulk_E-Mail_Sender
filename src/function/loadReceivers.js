21abc716832b9f09a5bd5b2a6d9d86228d59f7cc
Aniket-Shobhit <shobhitaniket3@gmail.com>
2022-08-26 16:38:53 +0530

    created json file

--- src/function/loadReceivers.js
+++ src/function/loadReceivers.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const loadReceivers = () => {
     try {
-        const fullPath = path.join(__dirname,"../receivers.json");
+        const fullPath = path.join(__dirname,"../../receivers.json");
         const dataBuffer = fs.readFileSync(fullPath);
         const dataJSON = dataBuffer.toString();
         return JSON.parse(dataJSON);
